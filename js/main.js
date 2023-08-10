130fea1b2900817f5faa794b58314dbf062215c0
dhiego1510 <dhiego1510@gmail.com>
2023-08-10 09:12:19 -0500

    arreglando error de suma

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let a=Number(prompt("Ingresa numero")),
-    b=Number(prompt("Ingresa numero"));
+    b=Number(prompt("Ingresa numero")),
+    resultado=a+b;
 
-alert("Suma total es: "+a+b);
+alert("Suma total es: " + resultado);
